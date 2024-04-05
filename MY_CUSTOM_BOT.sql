da02e245fee334e708dd32b99e0bcee7973943a7
estheraruti <128743038+estheraruti@users.noreply.github.com>
2024-04-05 18:36:10 -0400

    sql to create database

---
+++ MY_CUSTOM_BOT.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE MY_CUSTOM_BOT;
+
+CREATE TABLE MY_CUSTOM_BOT.search (
+    search_term VARCHAR(255),
+    search_output VARCHAR(255),
+    url_results VARCHAR(255),
+    term_in_url VARCHAR(255)
+);
+
